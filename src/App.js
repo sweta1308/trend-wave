b5bdbb533650ff4ea10bd137be32a40bee3b4353
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-09 16:27:46 +0530

    feat: add routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { Route, Routes } from 'react-router';
 import './App.css';
+import { Header } from './pages/header/Header';
+import { Home } from './pages/home/Home';
+import { Login } from './pages/login/Login';
+import { Signup } from './pages/signup/Signup';
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path='/'element={<Home />} />
+        <Route path='/register' element={<Header />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+      </Routes>
     </div>
   );
 }
