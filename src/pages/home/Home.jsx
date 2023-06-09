d65eab880e8f9f7197885811fb89e35c35b6345f
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-09 16:36:24 +0530

    docs: update home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,3 +1,11 @@
+import { useNavigate } from "react-router"
+
 export const Home  = () => {
-    return <h1>Home</h1>
+    const navigate = useNavigate();
+    return (
+        <>
+            <h1>Home</h1>
+            <button className="bg-black text-white" onClick={()=> navigate('/register')}>Register</button>
+        </>
+    )
 }
