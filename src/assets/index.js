25d2b4e2cb651c9a8c541354d77d723093807fb3
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-09 16:27:55 +0530

    docs: add images

---
+++ src/assets/index.js
@@ -0,0 +1,5 @@
+const logo = 'https://res.cloudinary.com/sweta-agarwalla/image/upload/v1686295154/trendwave-1_dhn5jj.png';
+const header = 'https://res.cloudinary.com/sweta-agarwalla/image/upload/v1686295261/undraw_social_networking_re_i1ex_o4fw8j.svg';
+const login = 'https://res.cloudinary.com/sweta-agarwalla/image/upload/v1686298938/Hand-taking-shot-of-aesthetic-wall-background_czpoiv.jpg';
+
+export {logo, header, login};
