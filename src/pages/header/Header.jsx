6ab1a7dee7f0712a7c662beec0bab90b66bdf8b7
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-09 16:28:59 +0530

    feat: add header page

---
+++ src/pages/header/Header.jsx
@@ -0,0 +1,22 @@
+import { useNavigate } from "react-router";
+import { header, logo } from "../../assets";
+import './Header.css'
+
+export const Header = () => {
+  const navigate = useNavigate();
+  return (
+    <>
+      <div className="landing-container py-24 px-54 flex justify-evenly xl:px-56 lg:px-32 md:flex-col-reverse md:items-center">
+        <div className="w-240 md:text-center">
+          <img src={logo} alt="logo" className="w-60 pb-20 md:m-auto md:py-12 pb-16" />   
+          <p className="uppercase text-xs"><span className="text-4xl text-gray-500 font-semibold md:text-3xl">Follow</span>people around the globe</p>
+          <p className="uppercase text-xs"><span className="text-4xl text-gray-500 font-semibold md:text-3xl">Connect</span>with yor friends</p>
+          <p className="uppercase text-xs"><span className="text-4xl text-gray-500 font-semibold md:text-3xl">Share</span>what you are thinking</p>
+          <button onClick={() => navigate('/signup')} className="button-primary">Join Now</button>
+          <p className="text-sm">Already have an account? <span className="sign-up" onClick={() => navigate('/login')}>Log In</span></p>
+        </div>
+        <img src={header} alt="header" className="header w-96 xl:w-80 lg:w-64"  />
+      </div>
+    </>
+  );
+};
