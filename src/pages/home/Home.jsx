e6b4dcba0bf5187e0353097cdba7dbb73505c2d0
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-11 16:42:04 +0530

    docs: update home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,17 @@
-import { useNavigate } from "react-router"
+import { Navbar } from "../../components/navbar/Navbar";
+import { Sidenav } from "../../components/sidenav/Sidenav";
+import './Home.css';
 
-export const Home  = () => {
-    const navigate = useNavigate();
-    return (
-        <>
-            <h1>Home</h1>
-            <button className="bg-black text-white" onClick={()=> navigate('/register')}>Register</button>
-        </>
-    )
-}
+export const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="flex">
+        <Sidenav />
+        <div className="content bg-primary-lightest relative">
+          <h1>Home</h1>
+        </div>
+      </div>
+    </>
+  );
+};
