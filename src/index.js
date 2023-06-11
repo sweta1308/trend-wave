eac8d7e64a91adb68e607d29afde3d4dce366869
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-11 16:40:55 +0530

    docs: add auth provider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import { makeServer } from "./server";
 
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./context/auth-context";
 
 // Call make Server
 makeServer();
@@ -13,7 +14,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
