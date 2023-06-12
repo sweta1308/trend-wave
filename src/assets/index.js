e07202a72dcbcbb14e3320d3208302859d0f7760
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-13 00:09:57 +0530

    docs: add images

--- src/assets/index.js
+++ src/assets/index.js
@@ -11,6 +11,8 @@ const blackDress =
   "https://res.cloudinary.com/sweta-agarwalla/image/upload/v1686550126/xl-vrro-24-black-drs-ifr-19-drs-black-color-one-piece-sexy-one-original-imagdfd7bjhzsgvn_anxtse.jpg";
 const post =
   "https://res.cloudinary.com/sweta-agarwalla/image/upload/v1686556867/photo-1557002666-513ca8eaa3c8_ozpdgt.jpg";
+const girlImage = 
+  "https://res.cloudinary.com/sweta-agarwalla/image/upload/v1686594530/f5c4feb96004b7063cd42c5a96f0d9fe--bungalows-_czg2xq.jpg";
 
 const girlAvatar1 =
   "https://res.cloudinary.com/sweta-agarwalla/image/upload/v1686567947/lsqovdnnbbcrsk4esggb.png";
@@ -18,10 +20,14 @@ const girlAvatar2 =
   "https://res.cloudinary.com/sweta-agarwalla/image/upload/v1686567964/dy0xhirdqfty9yr7rtne.jpg";
 const girlAvatar3 =
   "https://res.cloudinary.com/sweta-agarwalla/image/upload/v1686568144/images_gikgfr.png";
+const girlAvatar4 =
+  "https://res.cloudinary.com/sweta-agarwalla/image/upload/v1686594186/ggjr30cbxclnyxkdepjh.jpg";
 const boyAvatar1 =
   "https://res.cloudinary.com/sweta-agarwalla/image/upload/v1686568052/ojq7xh3aoe3a5kikgggi.jpg";
 const boyAvatar2 =
   "https://res.cloudinary.com/sweta-agarwalla/image/upload/v1686568094/hachocsneemwbzutb3ex.jpg";
+const boyAvatar3 =
+  "https://res.cloudinary.com/sweta-agarwalla/image/upload/v1686593850/cute-boy-avatar-illustration_637684-31_gxficd.jpg";
 
 export {
   logo,
@@ -30,9 +36,12 @@ export {
   attireBanner,
   blackDress,
   post,
+  girlImage,
   girlAvatar1,
   girlAvatar3,
   girlAvatar2,
+  girlAvatar4,
   boyAvatar1,
   boyAvatar2,
+  boyAvatar3,
 };
