86a37f39913cea21b8bfa5acc174d799e30b1406
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-13 00:10:18 +0530

    style: add padding bottom

--- src/pages/Bookmark.jsx
+++ src/pages/Bookmark.jsx
@@ -9,7 +9,7 @@ export const Bookmark = () => {
       <Navbar />
       <div className="flex">
         <Sidenav />
-        <div className="py-5 mx-2 relative left-[15%] w-[65%] flex flex-col items-center bg-primary-lightest rounded-xl lg:left-[30%] lg:w-[65%] md:left-0 md:w-full">
+        <div className="py-5 mx-2 relative left-[15%] w-[65%] flex flex-col items-center bg-primary-lightest rounded-xl lg:left-[30%] lg:w-[65%] md:left-0 md:w-full md:pb-[110px]">
           <h1>Bookmark</h1>
         </div>
         <RightNav />
