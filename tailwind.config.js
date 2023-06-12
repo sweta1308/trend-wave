b78a92d6bb1c9fc11afcad4b99deb238234154f9
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-12 23:17:05 +0530

    docs: add screens for resposiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,21 +3,21 @@ module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     screens: {
-      '2xl': {'max': '1535px'},
+      "2xl": { max: "1535px" },
       // => @media (max-width: 1535px) { ... }
 
-      'xl': {'max': '1279px'},
+      xl: { max: "1279px" },
       // => @media (max-width: 1279px) { ... }
 
-      'lg': {'max': '1023px'},
+      lg: { max: "1023px" },
       // => @media (max-width: 1023px) { ... }
 
-      'md': {'max': '767px'},
+      md: { max: "767px" },
       // => @media (max-width: 767px) { ... }
 
-      'sm': {'max': '639px'},
+      sm: { max: "639px" },
       // => @media (max-width: 639px) { ... }
-      'xs' : {'max' : '470px'}
+      xs: { max: "470px" },
     },
     extend: {
       colors: {
@@ -25,8 +25,9 @@ module.exports = {
         "primary-dark": "var(--primary-dark)",
         "primary-darker": "var(--primary-darker)",
         "primary-lightest": "var(--primary-lightest)",
-      }
+        "gray-text": "var(--gray-text)",
+      },
     },
   },
   plugins: [],
-}
+};
