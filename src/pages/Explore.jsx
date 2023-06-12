3f716fd5c94aae2fa35546848991dd5cc4c5d4f6
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-13 00:10:34 +0530

    style: add padding bottom

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -14,7 +14,7 @@ export const Explore = () => {
       <Navbar />
       <div className="flex relative">
         <Sidenav />
-        <div className="py-5 mx-2 relative left-[15%] w-[65%] flex flex-col items-center bg-primary-lightest rounded-xl lg:left-[30%] lg:w-[65%] md:left-0 md:w-full">
+        <div className="py-5 mx-2 relative left-[15%] w-[65%] flex flex-col items-center bg-primary-lightest rounded-xl lg:left-[30%] lg:w-[65%] md:left-0 md:w-full md:pb-[110px]">
           {postState.postLoading && (
             <PulseLoader color="var(--primary-color)" size={30} />
           )}
