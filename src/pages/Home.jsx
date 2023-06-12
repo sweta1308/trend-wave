a32fbbf8f0930f4b145270df2919c21f4b591299
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-12 23:15:53 +0530

    feat: add home/feed page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import { Navbar } from "../components/Navbar";
+import { RightNav } from "../components/RightNav";
+import { Sidenav } from "../components/Sidenav";
+
+export const Home = () => {
+  document.title = "Trend Wave | Feed";
+  return (
+    <>
+      <Navbar />
+      <div className="flex">
+        <Sidenav />
+        <div className="py-5 mx-2 relative left-[15%] w-[65%] flex flex-col items-center bg-primary-lightest rounded-xl lg:left-[30%] lg:w-[65%] md:left-0 md:w-full">
+          <h1>Home</h1>
+        </div>
+        <RightNav />
+      </div>
+    </>
+  );
+};
