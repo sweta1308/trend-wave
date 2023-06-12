af139c0b045dfac317c6816dbcbb532f85cedd9e
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-12 23:12:26 +0530

    docs: add images

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,38 @@
-const logo = 'https://res.cloudinary.com/sweta-agarwalla/image/upload/v1686295154/trendwave-1_dhn5jj.png';
-const header = 'https://res.cloudinary.com/sweta-agarwalla/image/upload/v1686295261/undraw_social_networking_re_i1ex_o4fw8j.svg';
-const login = 'https://res.cloudinary.com/sweta-agarwalla/image/upload/v1686298938/Hand-taking-shot-of-aesthetic-wall-background_czpoiv.jpg';
+const logo =
+  "https://res.cloudinary.com/sweta-agarwalla/image/upload/v1686295154/trendwave-1_dhn5jj.png";
+const header =
+  "https://res.cloudinary.com/sweta-agarwalla/image/upload/v1686295261/undraw_social_networking_re_i1ex_o4fw8j.svg";
+const login =
+  "https://res.cloudinary.com/sweta-agarwalla/image/upload/v1686298938/Hand-taking-shot-of-aesthetic-wall-background_czpoiv.jpg";
 
-export {logo, header, login};
+const attireBanner =
+  "https://res.cloudinary.com/sweta-agarwalla/image/upload/v1686548483/My_project-1_3_hwgedx.png";
+const blackDress =
+  "https://res.cloudinary.com/sweta-agarwalla/image/upload/v1686550126/xl-vrro-24-black-drs-ifr-19-drs-black-color-one-piece-sexy-one-original-imagdfd7bjhzsgvn_anxtse.jpg";
+const post =
+  "https://res.cloudinary.com/sweta-agarwalla/image/upload/v1686556867/photo-1557002666-513ca8eaa3c8_ozpdgt.jpg";
+
+const girlAvatar1 =
+  "https://res.cloudinary.com/sweta-agarwalla/image/upload/v1686567947/lsqovdnnbbcrsk4esggb.png";
+const girlAvatar2 =
+  "https://res.cloudinary.com/sweta-agarwalla/image/upload/v1686567964/dy0xhirdqfty9yr7rtne.jpg";
+const girlAvatar3 =
+  "https://res.cloudinary.com/sweta-agarwalla/image/upload/v1686568144/images_gikgfr.png";
+const boyAvatar1 =
+  "https://res.cloudinary.com/sweta-agarwalla/image/upload/v1686568052/ojq7xh3aoe3a5kikgggi.jpg";
+const boyAvatar2 =
+  "https://res.cloudinary.com/sweta-agarwalla/image/upload/v1686568094/hachocsneemwbzutb3ex.jpg";
+
+export {
+  logo,
+  header,
+  login,
+  attireBanner,
+  blackDress,
+  post,
+  girlAvatar1,
+  girlAvatar3,
+  girlAvatar2,
+  boyAvatar1,
+  boyAvatar2,
+};
