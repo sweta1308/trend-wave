44f961094417853ffed0f5cf4e32ad16552ed062
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-12 23:15:24 +0530

    feat: add bookmark page

---
+++ src/pages/Bookmark.jsx
@@ -0,0 +1,19 @@
+import { Navbar } from "../components/Navbar";
+import { RightNav } from "../components/RightNav";
+import { Sidenav } from "../components/Sidenav";
+
+export const Bookmark = () => {
+  document.title = "Trend Wave | Bookmark";
+  return (
+    <>
+      <Navbar />
+      <div className="flex">
+        <Sidenav />
+        <div className="py-5 mx-2 relative left-[15%] w-[65%] flex flex-col items-center bg-primary-lightest rounded-xl lg:left-[30%] lg:w-[65%] md:left-0 md:w-full">
+          <h1>Bookmark</h1>
+        </div>
+        <RightNav />
+      </div>
+    </>
+  );
+};
