b215807a2e1bf76245059d2df969cb5a197f36e8
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-12 23:13:58 +0530

    feat: add right nav

---
+++ src/components/RightNav.jsx
@@ -0,0 +1,7 @@
+export const RightNav = () => {
+  return (
+    <div className="fixed left-[83%] px-3 py-5 border-l-2 border-solid border-black text-center min-h-screen lg:hidden">
+      <h1>Bye</h1>
+    </div>
+  );
+};
