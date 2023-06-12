40a196fb567225ea36ab31858a9f9e65f8f9f9e3
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-12 23:12:17 +0530

    feat: add routes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
-import { Route, Routes } from 'react-router';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import './App.css';
-import { RequireAuth } from './components/RequireAuth';
-import { Header } from './pages/header/Header';
-import { Home } from './pages/home/Home';
-import { Login } from './pages/login/Login';
-import { Signup } from './pages/signup/Signup';
-import Mockman from 'mockman-js';
-import { Explore } from './pages/explore/Explore';
-import { Profile } from './pages/profile/Profile';
+import { Route, Routes } from "react-router";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+import { RequireAuth } from "./components/RequireAuth";
+import { Header } from "./pages/Header";
+import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+import { Signup } from "./pages/Signup";
+import Mockman from "mockman-js";
+import { Explore } from "./pages/Explore";
+import { Profile } from "./pages/Profile";
+import { Bookmark } from "./pages/Bookmark";
 
 function App() {
   return (
     <div className="App">
-      <ToastContainer position="top-right" autoClose={2000}  />
+      <ToastContainer position="top-right" autoClose={2000} />
       <Routes>
-        <Route path='/register' element={<Header />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<Signup />} />
+        <Route path="/register" element={<Header />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route element={<RequireAuth />}>
-          <Route path='/'element={<Home />} />
-          <Route path='/explore' element={<Explore />} />
-          <Route path='/profile' element={<Profile />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/bookmark" element={<Bookmark />} />
+          <Route path="/profile" element={<Profile />} />
         </Route>
-        <Route path='/mockman' element={<Mockman />} />
+        <Route path="/mockman" element={<Mockman />} />
       </Routes>
     </div>
   );
