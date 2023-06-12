84d7715aa316d3cbdd239dbc1256be05b9e8cdc9
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-12 23:12:40 +0530

    docs: add data for posts

--- src/backend/db/posts.js
+++ src/backend/db/posts.js
@@ -1,5 +1,5 @@
-import { v4 as uuid } from "uuid";
 import { formatDate } from "../utils/authUtils";
+import { attireBanner, blackDress, post } from "../../assets";
 
 /**
  * Posts can be added here.
@@ -8,29 +8,171 @@ import { formatDate } from "../utils/authUtils";
 
 export const posts = [
   {
-    _id: uuid(),
-    content:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+    _id: "hsjsnkc_2739_kse3_ksnkqwijdm",
+    content: "Visit our website for exclusive offers.",
+    imageUrl: attireBanner,
     likes: {
-      likeCount: 0,
+      likeCount: 23,
       likedBy: [],
       dislikedBy: [],
     },
+    comments: [
+      {
+        _id: "asjwindj_2718_jsjj_jqkwkn82",
+        username: "johndoe",
+        text: "Great collection. Quality is also really good.",
+        votes: {
+          upvotedBy: [],
+          downvotedBy: [],
+        },
+      },
+    ],
     username: "adarshbalika",
-    createdAt: formatDate(),
+    createdAt: "2023-02-16T10:55:06+05:30",
     updatedAt: formatDate(),
   },
+
   {
-    _id: uuid(),
-    content:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+    _id: "jshishi7_8282_je3s_kwjoqjw83js",
+    content: "Florals? For Spring? Groundbreaking.",
+    imageUrl: '',
     likes: {
-      likeCount: 0,
+      likeCount: 10,
       likedBy: [],
       dislikedBy: [],
     },
-    username: "shubhamsoni",
-    createdAt: formatDate(),
+    comments: [
+      {
+        _id: "njsjkw82n_i83s_882j_ojiu38jsk9",
+        username: "swetaagarwal",
+        text: "Breathtaking as well😉.",
+        votes: {
+          upvotedBy: [],
+          downvotedBy: [],
+        },
+      },
+      {
+        _id: "kskxmswo_92io_2i2j_lksoiii9332",
+        username: "janedoe",
+        text: "🔥🤩.",
+        votes: {
+          upvotedBy: [],
+          downvotedBy: [],
+        },
+      },
+    ],
+    username: "aryajoshi",
+    createdAt: "2022-09-29T10:15:21+05:30",
+    updatedAt: formatDate(),
+  },
+
+  {
+    _id: "owjin3jj9_o223_ke93_kskoi3os",
+    content: "Simplicity is the keynote of all true elegance.",
+    imageUrl: '',
+    likes: {
+      likeCount: 5,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    comments: [],
+    username: "adarshbalika",
+    createdAt: "2022-01-16:15:02+18:30",
+    updatedAt: formatDate(),
+  },
+
+  {
+    _id: "oakmkzsw_8dj2_w229_kjskl939s",
+    content: "When in doubt, wear black🖤.",
+    imageUrl: blackDress,
+    likes: {
+      likeCount: 49,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    comments: [
+      {
+        _id: "ksokmkkxw_82ji_82nn_knwiu983ns9",
+        username: "adarshbalika",
+        text: "Black Love.",
+        votes: {
+          upvotedBy: [],
+          downvotedBy: [],
+        },
+      }
+    ],
+    username: "swetaagarwal",
+    createdAt: "2023-06-02T10:38:10+05:30",
+    updatedAt: formatDate(),
+  },
+
+  {
+    _id: "owjin3jj9_o223_ke93_kskoi3os",
+    content: "Timeless wardrobe favourite <3.",
+    imageUrl: post,
+    likes: {
+      likeCount: 92,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    comments: [],
+    username: "janedoe",
+    createdAt: "2023-06-01T10:55:01+18:30",
+    updatedAt: formatDate(),
+  },
+
+  {
+    _id: "oijsiuwn_928j_jwn8_ksknjnjn8",
+    content: "In a world full of trends, I want to remain a classic.",
+    imageUrl: "",
+    likes: {
+      likeCount: 28,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    comments: [],
+    username: "swetaagarwal",
+    createdAt: "2022-10-10T10:52:13+05:30",
+    updatedAt: formatDate(),
+  },
+
+  {
+    _id: "dkjheke9_n38d_ksi2_kmskni2j",
+    content: "True happiness is not attained through self-gratification, but through fidelity to a worthy purpose.",
+    imageUrl: "",
+    likes: {
+      likeCount: 16,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    comments: [],
+    username: "swetaagarwal",
+    createdAt: "2023-01-16T10:17:19+05:30",
+    updatedAt: formatDate(),
+  },
+
+  {
+    _id: "nzsjsjje_i3j8_8320_ksnken3oo83",
+    content: "It is better to fail in originality than to succeed in imitation.",
+    imageUrl: '',
+    likes: {
+      likeCount: 72,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    comments: [
+      {
+        _id: "ismsppxeo_983j_83jd_knskne48",
+        username: "janedoe",
+        text: "True",
+        votes: {
+          upvotedBy: [],
+          downvotedBy: [],
+        },
+      },
+    ],
+    username: "aryajoshi",
+    createdAt: "2022-04-18T10:15:23+05:30",
     updatedAt: formatDate(),
   },
 ];
