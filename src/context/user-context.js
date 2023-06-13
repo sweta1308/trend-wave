cdf32c37cf531a1c08cee970c3845a79330dce4d
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-13 13:58:20 +0530

    feat: add loading state

--- src/context/user-context.js
+++ src/context/user-context.js
@@ -3,21 +3,25 @@ import { toast } from "react-toastify";
 import { userReducer } from "../reducer/user-reducer";
 import axios from "axios";
 import { useAuth } from "./auth-context";
+import { useState } from "react";
 
 const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
   const { authState } = useAuth();
   const [userState, userDispatch] = useReducer(userReducer, []);
+  const [userLoading, setUserLoading] = useState(false);
 
   const getUserData = async () => {
     try {
+      setUserLoading(true);
       const { data, status } = await axios({
         method: "GET",
         url: "/api/users",
       });
       if (status === 200 || status === 201) {
         userDispatch({ type: "GET_USER", payload: data?.users });
+        setUserLoading(false);
       }
     } catch (e) {
       toast.error(e.response.data.errors[0]);
@@ -32,7 +36,7 @@ export const UserProvider = ({ children }) => {
 
   return (
     <>
-      <UserContext.Provider value={{ userState }}>
+      <UserContext.Provider value={{ userState, userLoading }}>
         {children}
       </UserContext.Provider>
     </>
