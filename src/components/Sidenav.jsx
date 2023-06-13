01dc8626acf33908269eff700833a32df8ac692e
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-13 13:58:11 +0530

    style: update styles

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -5,17 +5,17 @@ export const Sidenav = () => {
   const { userLogout } = useAuth();
 
   const getStyles = ({ isActive }) => ({
-    backgroundColor: isActive ? "#566176" : "white",
+    backgroundColor: isActive ? "#377dff" : "white",
     borderRadius: isActive ? "10px" : "",
     color: isActive ? "white" : "",
   });
 
   return (
     <>
-      <div className="bg-white px-4 pr-1 pt-2 text-center min-h-screen fixed w-[15%] lg:w-[20%] md:flex md:justify-evenly md:w-full md:min-h-0 md:bottom-0 md:z-20 md:pt-2.5 md:py-1 md:pb-3.5 xs:text-sm">
+      <div className="bg-white px-4 pr-3 pt-2 text-left md:text-center min-h-screen fixed w-[15%] xl:text-[14px] lg:text-[16px] lg:w-[20%] md:flex md:justify-evenly md:w-full md:min-h-0 md:bottom-0 md:z-20 md:pt-2.5 md:py-1 md:pb-3.5 xs:text-sm">
         <NavLink
           style={getStyles}
-          className="block py-4 cursor-pointer hover:scale-110 md:px-3 xs:px-2"
+          className="block py-4 cursor-pointer md:px-3 xs:px-2 hover:text-primary-color pl-10 xl:pl-5"
           to="/"
         >
           <i class="fa-solid fa-grip"></i>{" "}
@@ -23,7 +23,7 @@ export const Sidenav = () => {
         </NavLink>
         <NavLink
           style={getStyles}
-          className="block py-4 cursor-pointer hover:scale-110 md:px-3  xs:px-2"
+          className="block py-4 cursor-pointer md:px-3 xs:px-2 hover:text-primary-color pl-10 xl:pl-5"
           to="/explore"
         >
           <i class="fa-regular fa-compass"></i>{" "}
@@ -31,7 +31,7 @@ export const Sidenav = () => {
         </NavLink>
         <NavLink
           style={getStyles}
-          className="block py-4 cursor-pointer hover:scale-110 md:px-3  xs:px-2"
+          className="block py-4 cursor-pointer md:px-3 xs:px-2 hover:text-primary-color pl-10 xl:pl-5"
           to="/bookmark"
         >
           <i class="fa-regular fa-bookmark"></i>{" "}
@@ -39,14 +39,14 @@ export const Sidenav = () => {
         </NavLink>
         <NavLink
           style={getStyles}
-          className="block py-4 cursor-pointer hover:scale-110 md:px-3  xs:px-2"
+          className="block py-4 cursor-pointer md:px-3 xs:px-2 hover:text-primary-color pl-10 xl:pl-5"
           to="/profile"
         >
           <i class="fa-regular fa-user"></i>{" "}
           <span className="ml-3 font-medium md:block md:ml-0">Profile</span>
         </NavLink>
         <p
-          className="block py-4 cursor-pointer hover:scale-110 md:px-3  xs:px-2"
+          className="block py-4 cursor-pointer md:px-3 xs:px-2 hover:text-primary-color pl-10 xl:pl-5"
           onClick={() => userLogout()}
         >
           <i class="fa-solid fa-arrow-right-from-bracket"></i>{" "}
