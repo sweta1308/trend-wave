f4620708cb82b7f10de99edea1d2cb1239fc1157
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-14 23:27:34 +0530

    feat: update avatar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import { useNavigate } from "react-router";
 import { logo } from "../assets";
 import { useAuth } from "../context/auth-context";
+import { usePost } from "../context/post-context";
 
 export const Navbar = () => {
   const { authState } = useAuth();
+  const { getUserPost } = usePost();
   const navigate = useNavigate();
   return (
     <>
@@ -11,12 +13,31 @@ export const Navbar = () => {
         <img
           src={logo}
           alt="logo"
-          className="w-44 cursor-pointer sm:w-36"
+          className="w-44 cursor-pointer sm:w-32"
           onClick={() => navigate("/")}
         />
-        <div className="flex justify-around items-center w-56 sm:w-44 xs:w-36">
-          <i class="fa-solid fa-moon"></i>
-          <p>{`${authState?.user?.firstName} ${authState?.user?.lastName}`}</p>
+        <div className="flex justify-around items-center w-56 xs:w-44">
+          <i className="fa-solid fa-moon fa-lg cursor-pointer"></i>
+          <div
+            onClick={() => {
+              getUserPost(authState?.user?.username);
+              navigate(`/profile/${authState?.user?.username}`);
+            }}
+            className="flex items-center cursor-pointer hover:underline hover:decoration-blue-400 hover:text-primary-color sm:text-[14px]"
+          >
+            <p>{`${authState?.user?.firstName} ${authState?.user?.lastName}`}</p>
+            {authState?.user?.avatarUrl ? (
+              <img
+                src={authState?.user?.avatarUrl}
+                alt="avatar"
+                className="w-[35px] h-[35px] ml-2 rounded-full"
+              />
+            ) : (
+              <div className="w-[35px] h-[35px] ml-2 text-bold rounded-full bg-primary-color text-white flex justify-center items-center">
+                {authState?.user?.firstName?.slice(0, 1)}
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </>
