e22866605d1ca154bdc99c00ebdc45c0bcd09198
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-14 23:28:06 +0530

    style: made responsive

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -15,41 +15,33 @@ export const Sidenav = () => {
       <div className="bg-white px-4 pr-3 pt-2 text-left md:text-center min-h-screen fixed w-[15%] xl:text-[14px] lg:text-[16px] lg:w-[20%] md:flex md:justify-evenly md:w-full md:min-h-0 md:bottom-0 md:z-20 md:pt-2.5 md:py-1 md:pb-3.5 xs:text-sm">
         <NavLink
           style={getStyles}
-          className="block py-4 cursor-pointer md:px-3 xs:px-2 hover:text-primary-color pl-10 xl:pl-5"
+          className="block py-2 cursor-pointer md:px-5 xs:px-2 hover:text-primary-color pl-10 xl:pl-5 md:text-xs"
           to="/"
         >
-          <i class="fa-solid fa-grip"></i>{" "}
+          <i className="fa-solid fa-grip md:text-base"></i>{" "}
           <span className="ml-3 font-medium md:block md:ml-0">Feed</span>
         </NavLink>
         <NavLink
           style={getStyles}
-          className="block py-4 cursor-pointer md:px-3 xs:px-2 hover:text-primary-color pl-10 xl:pl-5"
+          className="block py-2 cursor-pointer md:px-5 xs:px-2 hover:text-primary-color pl-10 xl:pl-5 md:text-xs"
           to="/explore"
         >
-          <i class="fa-regular fa-compass"></i>{" "}
+          <i className="fa-regular fa-compass md:text-base"></i>{" "}
           <span className="ml-3 font-medium md:block md:ml-0">Explore</span>
         </NavLink>
         <NavLink
           style={getStyles}
-          className="block py-4 cursor-pointer md:px-3 xs:px-2 hover:text-primary-color pl-10 xl:pl-5"
+          className="block py-2 cursor-pointer md:px-5 xs:px-2 hover:text-primary-color pl-10 xl:pl-5 md:text-xs"
           to="/bookmark"
         >
-          <i class="fa-regular fa-bookmark"></i>{" "}
+          <i className="fa-regular fa-bookmark md:text-base"></i>{" "}
           <span className="ml-3 font-medium md:block md:ml-0">Bookmarks</span>
         </NavLink>
-        <NavLink
-          style={getStyles}
-          className="block py-4 cursor-pointer md:px-3 xs:px-2 hover:text-primary-color pl-10 xl:pl-5"
-          to="/profile"
-        >
-          <i class="fa-regular fa-user"></i>{" "}
-          <span className="ml-3 font-medium md:block md:ml-0">Profile</span>
-        </NavLink>
         <p
-          className="block py-4 cursor-pointer md:px-3 xs:px-2 hover:text-primary-color pl-10 xl:pl-5"
+          className="block py-2 cursor-pointer md:px-5 xs:px-2 hover:text-primary-color pl-10 xl:pl-5 md:text-xs"
           onClick={() => userLogout()}
         >
-          <i class="fa-solid fa-arrow-right-from-bracket"></i>{" "}
+          <i className="fa-solid fa-arrow-right-from-bracket md:text-base"></i>{" "}
           <span className="ml-3 font-medium md:block md:ml-0">Log Out</span>
         </p>
       </div>
