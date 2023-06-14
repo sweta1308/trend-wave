ed87ff6096b5d8a9a18de46791b7a2c90d2eca10
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-14 23:28:21 +0530

    feat: update post apis

--- src/context/post-context.js
+++ src/context/post-context.js
@@ -13,6 +13,7 @@ export const PostProvider = ({ children }) => {
   const initialState = {
     postLoading: false,
     post: [],
+    userPost: [],
   };
   const [postState, postDispatch] = useReducer(postReducer, initialState);
   const { authState } = useAuth();
@@ -33,6 +34,50 @@ export const PostProvider = ({ children }) => {
     }
   };
 
+  const getUserPost = async (username) => {
+    try {
+      const { data, status } = await axios({
+        method: "GET",
+        url: `/api/posts/user/${username}`,
+      });
+      if (status === 200 || status === 201) {
+        postDispatch({ type: "USER_POST", payload: data?.posts });
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const likePost = async (postId) => {
+    try {
+      const { data, status } = await axios({
+        method: "POSt",
+        url: `/api/posts/like/${postId}`,
+        headers: { authorization: authState?.token },
+      });
+      if (status === 200 || status === 201) {
+        postDispatch({ type: "GET_POST", payload: data?.posts });
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const dislikePost = async (postId) => {
+    try {
+      const { data, status } = await axios({
+        method: "POSt",
+        url: `/api/posts/dislike/${postId}`,
+        headers: { authorization: authState?.token },
+      });
+      if (status === 200 || status === 201) {
+        postDispatch({ type: "GET_POST", payload: data?.posts });
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   useEffect(() => {
     if (authState.token) {
       getPostData();
@@ -40,7 +85,9 @@ export const PostProvider = ({ children }) => {
   }, [authState.token]);
 
   return (
-    <PostContext.Provider value={{ postState }}>
+    <PostContext.Provider
+      value={{ postState, getUserPost, likePost, dislikePost }}
+    >
       {children}
     </PostContext.Provider>
   );
