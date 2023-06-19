5bba88e7567d9622de57f7fd9f45199c3ef088e9
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-19 22:17:04 +0530

    feat: add new post component in home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { Navbar } from "../components/Navbar";
+import { NewPost } from "../components/NewPost";
 import { RightNav } from "../components/RightNav";
 import { Sidenav } from "../components/Sidenav";
 
@@ -10,7 +11,7 @@ export const Home = () => {
       <div className="flex">
         <Sidenav />
         <div className="py-5 mx-5 relative left-[15%] w-[65%] flex flex-col items-center bg-primary-lightest rounded-xl lg:left-[30%] lg:w-[65%] md:left-0 md:w-full md:pb-[110px]">
-          <h1>Home</h1>
+          <NewPost />
         </div>
         <RightNav />
       </div>
