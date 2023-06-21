628efd0611419fb93fb67ce274b1596ae20d2efe
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-21 13:22:27 +0530

    feat: add modal in profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -10,6 +10,7 @@ import { useEffect } from "react";
 import axios from "axios";
 import { usePost } from "../context/post-context";
 import { DisplayPost } from "../components/DisplayPost";
+import { FollowCount } from "../components/FollowCount";
 
 export const Profile = () => {
   document.title = "Trend Wave | Profile";
@@ -19,6 +20,10 @@ export const Profile = () => {
   const { userState, followUser, unfollowUser } = useUser();
   const [userData, setUserData] = useState({});
   const [dataLoading, setDataLoading] = useState(false);
+  const [showModal, setShowModal] = useState({
+    show: false,
+    type: "",
+  });
 
   const getUserDetails = async () => {
     try {
@@ -48,120 +53,153 @@ export const Profile = () => {
       ?.followers.some((user) => user._id === authState?.user?._id);
 
   return (
-    <>
-      <Navbar />
-      <div className="flex">
-        <Sidenav />
-        <div className="py-5 mx-5 relative left-[15%] w-[65%] flex flex-col items-center bg-primary-lightest rounded-xl lg:left-[30%] lg:w-[65%] md:left-0 md:w-full  md:pb-[110px]">
-          {dataLoading ? (
-            <PulseLoader color="var(--primary-color)" size={30} />
-          ) : (
-            <div className="w-[500px] md:w-[380px] xs:w-[320px]">
-              <div className="flex items-center justify-between xs:text-[14px]">
-                <div className="flex items-center">
-                  {userData?.avatarUrl ? (
-                    <img
-                      src={userData?.avatarUrl}
-                      alt="avatar"
-                      className="w-[60px] h-[60px] mr-2 rounded-full"
-                    />
-                  ) : (
-                    <div className="w-[60px] text-2xl h-[60px] mr-2 rounded-full bg-primary-color text-white flex justify-center items-center">
-                      {authState?.user?.firstName?.slice(0, 1)}
+    <div className="relative">
+      {showModal.show && (
+        <FollowCount
+          arr={
+            showModal.type === "Following"
+              ? userData.following
+              : userData.followers
+          }
+          setShowModal={setShowModal}
+          showModal={showModal}
+        />
+      )}
+      <div style={{ filter: showModal.show ? "blur(8px)" : "" }}>
+        <Navbar />
+        <div className="flex">
+          <Sidenav />
+          <div className="py-5 mx-5 relative left-[15%] w-[65%] flex flex-col items-center bg-primary-lightest rounded-xl lg:left-[30%] lg:w-[65%] md:left-0 md:w-full  md:pb-[110px]">
+            {dataLoading ? (
+              <PulseLoader color="var(--primary-color)" size={30} />
+            ) : (
+              <div className="w-[500px] md:w-[380px] xs:w-[320px]">
+                <div className="flex items-center justify-between xs:text-[14px]">
+                  <div className="flex items-center">
+                    {userData?.avatarUrl ? (
+                      <img
+                        src={userData?.avatarUrl}
+                        alt="avatar"
+                        className="w-[60px] h-[60px] mr-2 rounded-full"
+                      />
+                    ) : (
+                      <div className="w-[60px] text-2xl h-[60px] mr-2 rounded-full bg-primary-color text-white flex justify-center items-center">
+                        {authState?.user?.firstName?.slice(0, 1)}
+                      </div>
+                    )}
+
+                    <div>
+                      <h1 className="font-bold">{`${userData?.firstName} ${userData?.lastName}`}</h1>
+                      <p className="text-sm">@{userData?.username}</p>
                     </div>
-                  )}
-
-                  <div>
-                    <h1 className="font-bold">{`${userData?.firstName} ${userData?.lastName}`}</h1>
-                    <p className="text-sm">@{userData?.username}</p>
                   </div>
+
+                  {userData?.username === authState?.user?.username ? (
+                    <button className="px-4 py-2 hover:text-primary-color rounded-lg">
+                      <i className="fa-solid fa-pen fa-md"></i>
+                    </button>
+                  ) : isFollowed(userData?._id) ? (
+                    <button
+                      onClick={() => unfollowUser(userData?._id)}
+                      className=" text-primary-color border-2 border-primary-color text-sm px-4 py-2 hover:bg-primary-color hover:text-white rounded-lg"
+                    >
+                      Following
+                    </button>
+                  ) : (
+                    <button
+                      onClick={() => followUser(userData?._id)}
+                      className="bg-primary-color text-white text-sm px-4 py-2 hover:bg-primary-dark rounded-lg"
+                    >
+                      <i className="fa-solid fa-plus fa-xs"></i> Follow
+                    </button>
+                  )}
                 </div>
 
-                {userData?.username === authState?.user?.username ? (
-                  <button className="px-4 py-2 hover:text-primary-color rounded-lg">
-                    <i className="fa-solid fa-pen fa-md"></i>
-                  </button>
-                ) : isFollowed(userData?._id) ? (
-                  <button
-                    onClick={() => unfollowUser(userData?._id)}
-                    className=" text-primary-color border-2 border-primary-color text-sm px-4 py-2 hover:bg-primary-color hover:text-white rounded-lg"
-                  >
-                    Following
-                  </button>
-                ) : (
-                  <button
-                    onClick={() => followUser(userData?._id)}
-                    className="bg-primary-color text-white text-sm px-4 py-2 hover:bg-primary-dark rounded-lg"
-                  >
-                    <i className="fa-solid fa-plus fa-xs"></i> Follow
-                  </button>
+                {userData?.bio && (
+                  <p className="my-px mt-2 text-[15px]">
+                    <i className="fa-brands fa-pagelines w-[60px] text-center text-xl"></i>
+                    {userData?.bio}
+                  </p>
                 )}
-              </div>
 
-              {userData?.bio && (
-                <p className="my-px mt-2 text-[15px]">
-                  <i className="fa-brands fa-pagelines w-[60px] text-center text-xl"></i>
-                  {userData?.bio}
+                {userData?.website && (
+                  <a
+                    href={userData?.website}
+                    target="_blank"
+                    className="text-[15px] my-2 hover:underline decoration-blue-400"
+                    rel="noreferrer"
+                  >
+                    <i class="fa-solid fa-link w-[60px] text-center"></i>
+                    {userData?.website}
+                  </a>
+                )}
+
+                <p className="my-2 text-[15px]">
+                  <i className="fa-regular fa-calendar w-[60px] text-center"></i>
+                  Joined{" "}
+                  {` ${new Date(userData?.createdAt)
+                    .toDateString()
+                    .split(" ")
+                    .slice(1, 4)
+                    .join(" ")}`}
                 </p>
-              )}
 
-              {userData?.website && (
-                <a
-                  href={userData?.website}
-                  target="_blank"
-                  className="text-[15px] my-2 hover:underline decoration-blue-400"
-                  rel="noreferrer"
-                >
-                  <i class="fa-solid fa-link w-[60px] text-center"></i>
-                  {userData?.website}
-                </a>
-              )}
-
-              <p className="my-2 text-[15px]">
-                <i className="fa-regular fa-calendar w-[60px] text-center"></i>
-                Joined{" "}
-                {` ${new Date(userData?.createdAt)
-                  .toDateString()
-                  .split(" ")
-                  .slice(1, 4)
-                  .join(" ")}`}
-              </p>
-
-              <div className="pl-[25px] flex justify-between w-[300px]">
-                <p>
-                  {postState?.userPost?.length +
-                    `${postState?.userPost?.length === 1 ? " Post" : " Posts"}`}
-                </p>
-                <p>
-                  {userData?.followers?.length +
-                    `${
-                      userData?.followers?.length === 1
-                        ? " Follower"
-                        : " Followers"
-                    }`}
-                </p>
-                <p>
-                  {userData?.following?.length +
-                    `${
-                      userData?.following?.length === 1
-                        ? " Following"
-                        : " Followings"
-                    }`}
-                </p>
-              </div>
-
-              <hr className="my-5" />
-
-              {postState?.userPost?.map((post) => (
-                <div>
-                  <DisplayPost userPost={post} />
+                <div className="pl-[25px] flex justify-between w-[300px]">
+                  <p>
+                    {postState?.userPost?.length +
+                      `${
+                        postState?.userPost?.length === 1 ? " Post" : " Posts"
+                      }`}
+                  </p>
+                  <p
+                    className="hover:underline cursor-pointer"
+                    onClick={() =>
+                      setShowModal((showModal) => ({
+                        ...showModal,
+                        show: true,
+                        type: "Followers",
+                      }))
+                    }
+                  >
+                    {userData?.followers?.length +
+                      `${
+                        userData?.followers?.length === 1
+                          ? " Follower"
+                          : " Followers"
+                      }`}
+                  </p>
+                  <p
+                    className="hover:underline cursor-pointer"
+                    onClick={() =>
+                      setShowModal((showModal) => ({
+                        ...showModal,
+                        show: true,
+                        type: "Following",
+                      }))
+                    }
+                  >
+                    {userData?.following?.length +
+                      `${
+                        userData?.following?.length === 1
+                          ? " Following"
+                          : " Followings"
+                      }`}
+                  </p>
                 </div>
-              ))}
-            </div>
-          )}
+
+                <hr className="my-5" />
+
+                {postState?.userPost?.map((post) => (
+                  <div>
+                    <DisplayPost userPost={post} />
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+          <RightNav />
         </div>
-        <RightNav />
       </div>
-    </>
+    </div>
   );
 };
