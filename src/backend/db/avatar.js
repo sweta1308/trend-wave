eab0a103f31b982dfdda3902478a453497fb4525
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-25 22:32:13 +0530

    docs: add avatar data

---
+++ src/backend/db/avatar.js
@@ -0,0 +1,11 @@
+export const avatar = [
+  "https://res.cloudinary.com/sweta-agarwalla/image/upload/v1687671770/images_pisuoo.jpg",
+  "https://res.cloudinary.com/sweta-agarwalla/image/upload/v1687671494/images_lujn8d.png",
+  "https://res.cloudinary.com/sweta-agarwalla/image/upload/v1687671463/images_fatz0i.png",
+  "https://res.cloudinary.com/sweta-agarwalla/image/upload/v1687671559/images_ky1z9r.jpg",
+  "https://res.cloudinary.com/sweta-agarwalla/image/upload/v1687671835/images_wceilp.png",
+  "https://res.cloudinary.com/sweta-agarwalla/image/upload/v1687671664/images_fyigox.png",
+  "https://res.cloudinary.com/sweta-agarwalla/image/upload/v1687671642/images_q03uh5.jpg",
+  "https://res.cloudinary.com/sweta-agarwalla/image/upload/v1687671519/images_mytgbm.jpg",
+  "https://res.cloudinary.com/sweta-agarwalla/image/upload/v1687671710/images_iaqjwz.jpg",
+];
