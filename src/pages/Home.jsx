5dfd3699e09770ecdd7e470f345d9f289886d3cf
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-25 22:35:21 +0530

    feat: add filter component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,7 @@ import { useAuth } from "../context/auth-context";
 import { usePost } from "../context/post-context";
 import { useUser } from "../context/user-context";
 import { DisplayPost } from "../components/DisplayPost";
+import { FilterComponent } from "../components/FilterComponent";
 
 export const Home = () => {
   document.title = "Trend Wave | Feed";
@@ -23,6 +24,7 @@ export const Home = () => {
     );
     return followUsernameArr?.includes(username);
   });
+
   userFeed = [
     ...userFeed,
     ...followFeedPost,
@@ -31,7 +33,15 @@ export const Home = () => {
     ),
   ];
 
-  console.log(userFeed);
+  const sortAsTrending = () => {
+    userFeed = userFeed.sort(
+      (a, b) => b?.likes?.likeCount - a?.likes?.likeCount
+    );
+    return userFeed;
+  };
+
+  const sortAsLatest = () => {};
+
   return (
     <>
       <Navbar />
@@ -39,15 +49,22 @@ export const Home = () => {
         <Sidenav />
         <div className="py-5 mx-5 relative left-[15%] w-[65%] flex flex-col items-center bg-primary-lightest rounded-xl lg:left-[30%] lg:w-[65%] md:left-0 md:w-full md:pb-[110px]">
           <NewPost />
+          <FilterComponent
+            sortAsTrending={sortAsTrending}
+            sortAsLatest={sortAsLatest}
+          />
           {postState.postLoading && (
             <PulseLoader color="var(--primary-color)" size={30} />
           )}
           <div className="mt-[30px]">
-          {userFeed?.map((posts) => (
-            <div key={posts._id}>
-              <DisplayPost userPost={posts} />
-            </div>
-          ))}
+            {userFeed.length === 0 && (
+              <h1 className="font-bold text-3xl">No Posts to show.</h1>
+            )}
+            {userFeed?.map((posts) => (
+              <div key={posts._id}>
+                <DisplayPost userPost={posts} />
+              </div>
+            ))}
           </div>
         </div>
         <RightNav />
