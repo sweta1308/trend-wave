3ea4ae1ca2907ac9d3a3b223220532f54bdb5f11
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-25 22:33:02 +0530

    feat: add edit post function

--- src/components/EditDeleteModal.jsx
+++ src/components/EditDeleteModal.jsx
@@ -1,15 +1,15 @@
-export const EditDeleteModal = ({ deletePost }) => {
+export const EditDeleteModal = ({ editPost, deletePost }) => {
   return (
     <div className="text-sm bg-white p-3 w-[120px] shadow-lg absolute right-2 top-12">
       <p className="my-2 cursor-pointer hover:text-primary-color">
-        <i class="fa-solid fa-pencil"></i> Edit Post
+        <i onClick={editPost} className="fa-solid fa-pencil"></i> Edit Post
       </p>
       <hr />
       <p
         className="my-2 cursor-pointer hover:text-primary-color"
         onClick={deletePost}
       >
-        <i class="fa-solid fa-trash-can"></i> Delete Post
+        <i className="fa-solid fa-trash-can"></i> Delete Post
       </p>
     </div>
   );
