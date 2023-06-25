2668874abc521ba33905ef96ab5d3248f222bcaf
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-25 22:36:06 +0530

    fix: change the type name

--- src/reducer/user-reducer.js
+++ src/reducer/user-reducer.js
@@ -2,8 +2,8 @@ export const userReducer = (state, { type, payload }) => {
   switch (type) {
     case "GET_USER":
       return (state = payload);
-    case "UPDATE_USERDATA":
-      return state.map((user) => (user._id === payload._id ? payload : user));
+    case "UPDATE_USER":
+      return state.map((user) => (user._id === payload._id ? {...payload} : user));
     default:
       return state;
   }
