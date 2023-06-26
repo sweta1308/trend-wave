4a07e4d666cb3bf4a777f9d7590c32e5b358c0ac
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-26 20:33:20 +0530

    feat: add search bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,16 +8,25 @@ export const Navbar = () => {
   const { getUserPost } = usePost();
   const navigate = useNavigate();
   return (
-    <>
-      <div className="bg-white shadow-sm flex justify-between items-center px-14 sticky top-0 z-10 sm:px-3">
+    <div className="bg-white shadow-sm sticky top-0 z-30">
+      <div className="flex justify-between items-center px-14  sm:px-3">
         <img
           src={logo}
           alt="logo"
           className="w-44 cursor-pointer sm:w-32"
           onClick={() => navigate("/")}
         />
+        <div className="border border-gray-400 flex items-center rounded-lg md:hidden">
+          <i className="fa-solid fa-magnifying-glass mx-[10px]"></i>
+          <input
+            placeholder="Search User"
+            className="p-[10px] outline-none text-sm w-[350px] lg:w-[200px]"
+          />
+        </div>
+
         <div className="flex justify-around items-center w-56 xs:w-44">
           <i className="fa-solid fa-moon fa-lg cursor-pointer"></i>
+
           <div
             onClick={() => {
               getUserPost(authState?.user?.username);
@@ -34,6 +43,14 @@ export const Navbar = () => {
           </div>
         </div>
       </div>
-    </>
+
+      <div className="border border-gray-400 rounded-lg hidden md:block w-[full] mx-[50px] mb-[30px] sm:mx-[20px]">
+        <i className="fa-solid fa-magnifying-glass mx-[10px]"></i>
+        <input
+          placeholder="Search User"
+          className="p-[10px] outline-none text-sm"
+        />
+      </div>
+    </div>
   );
 };
