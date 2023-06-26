9b977d4e2e9a9a25fb90df24d615979e480bdb16
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-26 20:33:11 +0530

    feat: add filter component functionality

--- src/components/FilterComponent.jsx
+++ src/components/FilterComponent.jsx
@@ -1,12 +1,26 @@
-export const FilterComponent = ({ sortAsTrending, sortAsLatest }) => {
+import { usePost } from "../context/post-context";
+
+export const FilterComponent = () => {
+  const { postDispatch, postState } = usePost();
   return (
     <div>
-      <button onClick={sortAsTrending}>
-        <i className="fa-solid fa-fire"></i> Trending
-      </button>
-      <button onClick={sortAsLatest}>
-        <i className="fa-solid fa-clock-rotate-left"></i> Latest
-      </button>
+      <div className="flex justify-around w-[400px] mt-[20px] text-lg xs:w-[300px]">
+        <button
+          onClick={() => postDispatch({ type: "SORT", payload: "Trending" })}
+        >
+          <i className="fa-solid fa-fire"></i> Trending
+        </button>
+        <button
+          onClick={() => postDispatch({ type: "SORT", payload: "Latest" })}
+        >
+          <i className="fa-solid fa-clock-rotate-left"></i> Latest
+        </button>
+      </div>
+      {postState.sortBy && (
+        <h1 className="text-center mt-[30px] font-bold text-3xl text-primary-color underline decoration-black">
+          {postState?.sortBy} Posts
+        </h1>
+      )}
     </div>
   );
 };
