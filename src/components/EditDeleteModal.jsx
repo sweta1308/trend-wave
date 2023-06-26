e3cac775caaf7c60284656a3fef355c1b2b859a0
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-26 20:32:38 +0530

    feat: change onClick

--- src/components/EditDeleteModal.jsx
+++ src/components/EditDeleteModal.jsx
@@ -1,8 +1,11 @@
 export const EditDeleteModal = ({ editPost, deletePost }) => {
   return (
     <div className="text-sm bg-white p-3 w-[120px] shadow-lg absolute right-2 top-12">
-      <p className="my-2 cursor-pointer hover:text-primary-color">
-        <i onClick={editPost} className="fa-solid fa-pencil"></i> Edit Post
+      <p
+        onClick={editPost}
+        className="my-2 cursor-pointer hover:text-primary-color"
+      >
+        <i className="fa-solid fa-pencil"></i> Edit Post
       </p>
       <hr />
       <p
