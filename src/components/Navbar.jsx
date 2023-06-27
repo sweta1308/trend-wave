20cddcaf458dcc5d502eff23efca58690f2d2b6b
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-27 18:18:25 +0530

    feat: add dark mode and search modal function

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,58 @@
+import { useState } from "react";
 import { useNavigate } from "react-router";
 import { logo } from "../assets";
 import { useAuth } from "../context/auth-context";
 import { usePost } from "../context/post-context";
+import { SearchModal } from "./SearchModal";
+import { useTheme } from "../context/theme-context";
 
 export const Navbar = () => {
+  const { isDarkMode, setIsDarkMode } = useTheme();
   const { authState } = useAuth();
   const { getUserPost } = usePost();
+  const [searchInput, setsearchInput] = useState("");
+  const [showSearchModal, setShowSearchModal] = useState(false);
   const navigate = useNavigate();
   return (
     <div className="bg-white shadow-sm sticky top-0 z-30">
       <div className="flex justify-between items-center px-14  sm:px-3">
+        {showSearchModal && (
+          <SearchModal
+            searchInput={searchInput}
+            setShowSearchModal={setShowSearchModal}
+          />
+        )}
         <img
           src={logo}
           alt="logo"
           className="w-44 cursor-pointer sm:w-32"
           onClick={() => navigate("/")}
         />
-        <div className="border border-gray-400 flex items-center rounded-lg md:hidden">
+        <div
+          onClick={() => setShowSearchModal(true)}
+          className="border border-gray-400 flex items-center rounded-lg md:hidden"
+        >
           <i className="fa-solid fa-magnifying-glass mx-[10px]"></i>
           <input
             placeholder="Search User"
+            value={searchInput}
+            onChange={(e) => setsearchInput(e.target.value)}
             className="p-[10px] outline-none text-sm w-[350px] lg:w-[200px]"
           />
         </div>
 
         <div className="flex justify-around items-center w-56 xs:w-44">
-          <i className="fa-solid fa-moon fa-lg cursor-pointer"></i>
+          {isDarkMode ? (
+            <i
+              onClick={() => setIsDarkMode((prev) => !prev)}
+              className="fa-solid fa-sun fa-lg cursor-pointer"
+            ></i>
+          ) : (
+            <i
+              onClick={() => setIsDarkMode((prev) => !prev)}
+              className="fa-solid fa-moon fa-lg cursor-pointer"
+            ></i>
+          )}
 
           <div
             onClick={() => {
@@ -48,6 +75,8 @@ export const Navbar = () => {
         <i className="fa-solid fa-magnifying-glass mx-[10px]"></i>
         <input
           placeholder="Search User"
+          value={searchInput}
+          onChange={(e) => setsearchInput(e.target.value)}
           className="p-[10px] outline-none text-sm"
         />
       </div>
