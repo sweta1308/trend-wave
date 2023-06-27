3aaf89b7ecc2e53861612b63e17c225e1a4fd722
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-27 18:20:28 +0530

    feat: create search modal

---
+++ src/components/SearchModal.jsx
@@ -0,0 +1,56 @@
+import { useNavigate } from "react-router";
+import { useUser } from "../context/user-context";
+
+export const SearchModal = ({ searchInput, setShowSearchModal }) => {
+  const { userState } = useUser();
+  const navigate = useNavigate();
+  const filteredUser =
+    searchInput?.trim().length > 0 &&
+    userState?.filter(
+      (user) =>
+        user?.firstName
+          ?.toLowerCase()
+          .includes(searchInput.trim().toLowerCase()) ||
+        user?.lastName
+          ?.toLowerCase()
+          .includes(searchInput.trim().toLowerCase()) ||
+        user?.username?.toLowerCase().includes(searchInput.trim().toLowerCase())
+    );
+
+  return (
+    <div className="p-[20px] text-sm z-50 m-auto absolute top-[80px] bg-white shadow-lg left-[38%] w-[300px] xl:left-[30%] md:top-[150px] sm:top-[120px] sm:left-[25%] xs:left-[12%]">
+      <div>
+        {searchInput?.length >= 0 && (
+          <div>{filteredUser?.length === 0 && <h1>No Users Found</h1>}</div>
+        )}
+      </div>
+      <div>
+        {filteredUser?.length > 0 && (
+          <div>
+            {filteredUser?.map((user) => (
+              <div
+                className="flex items-center cursor-pointer my-[5px]"
+                onClick={() => {
+                  navigate(`/profile/${user?.username}`);
+                  setShowSearchModal(false);
+                }}
+              >
+                <img
+                  className="w-[50px] rounded-full mr-[10px]"
+                  src={user?.avatarUrl}
+                  alt="avatar"
+                />
+                <div>
+                  <h3>
+                    {user?.firstName} {user?.lastName}
+                  </h3>
+                  <p>@{user?.username}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
