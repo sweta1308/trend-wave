10824ac95ce063816f006625bb6c8f0fec4dc4d4
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-28 19:58:31 +0530

    feat: add comment context

---
+++ src/context/comment-context.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+import { useContext, createContext } from "react";
+import { useAuth } from "./auth-context";
+import { usePost } from "./post-context";
+
+const CommentContext = createContext();
+
+export const CommentProvider = ({ children }) => {
+  const { authState } = useAuth();
+  const { postDispatch } = usePost();
+
+  const addComments = async (postId, commentData) => {
+    try {
+      const { data, status } = await axios.post(
+        `/api/comments/add/${postId}`,
+        { commentData },
+        { headers: { authorization: authState?.token } }
+      );
+      if (status === 201 || status === 200) {
+        postDispatch({ type: "GET_POST", payload: data?.posts });
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const deleteComment = async (postId, commentId) => {
+    try {
+      const { data, status } = await axios({
+        method: "POST",
+        url: `/api/comments/delete/${postId}/${commentId}`,
+        headers: { authorization: authState?.token },
+      });
+      if (status === 201 || status === 200) {
+        postDispatch({ type: "GET_POST", payload: data?.posts });
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <CommentContext.Provider value={{ addComments, deleteComment }}>
+      {children}
+    </CommentContext.Provider>
+  );
+};
+
+export const useComment = () => useContext(CommentContext);
