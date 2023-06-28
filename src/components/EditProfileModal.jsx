97d9c1dcd636b8bbf4210bae56ba88f9050c933b
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-28 19:57:01 +0530

    feat: add material ui modal

--- src/components/EditProfileModal.jsx
+++ src/components/EditProfileModal.jsx
@@ -1,10 +1,9 @@
 import { useState } from "react";
 import { useUser } from "../context/user-context";
 import { Avatar } from "./AvatarModal";
-import { useAuth } from "../context/auth-context";
+import { Modal } from "@mui/material";
 
-export const EditProfile = ({ userObj, setShowEditModal }) => {
-  const { authState } = useAuth();
+export const EditProfile = ({ userObj, setShowEditModal, showEditModal }) => {
   const { editUserdata } = useUser();
   const [showAvatarModal, setShowAvatarModal] = useState(false);
   const [userValue, setUserValue] = useState({
@@ -14,6 +13,18 @@ export const EditProfile = ({ userObj, setShowEditModal }) => {
     bio: userObj?.bio,
     website: userObj?.website,
   });
+
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 340,
+    bgcolor: "background.paper",
+    boxShadow: 24,
+    p: 2,
+  };
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setUserValue((userValue) => ({ ...userValue, [name]: value }));
@@ -30,92 +41,100 @@ export const EditProfile = ({ userObj, setShowEditModal }) => {
         <Avatar
           setUserValue={setUserValue}
           setShowAvatarModal={setShowAvatarModal}
+          showAvatarModal={showAvatarModal}
         />
       )}
-      <div
-        style={{ filter: showAvatarModal ? "blur(10px)" : "" }}
-        className="p-[30px] block w-[400px] absolute left-[35%] top-[4%] z-20 bg-white md:left-[20%] sm:left-[10%] xs:w-[320px]"
+      <Modal
+        open={showEditModal}
+        onClose={() => setShowEditModal(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
       >
-        <h1 className="font-bold mb-[15px]">EDIT PROFILE</h1>
-        <form className="flex flex-col" onSubmit={submitHandler}>
-          <img
-            src={userValue?.avatarUrl}
-            alt="avatar"
-            className="w-[100px] h-[100px] m-auto rounded-full"
-          />
-
-          <button
-            onClick={(e) => {
-              e.preventDefault();
-              setShowAvatarModal(true);
-            }}
-            className="bg-primary-color text-white my-[6px] mb-[20px] p-[7px] hover:bg-primary-dark"
-          >
-            Update Avatar
-          </button>
-          <div className="flex xs:flex-col">
-            <div className="xs:mb-[10px]">
-              <label className="xs:mr-[10px]" for="first-name">
-                First Name
-              </label>
-              <input
-                value={userValue?.firstName}
-                id="first-name"
-                name="firstName"
-                className="text-sm w-[160px] border border-gray-300 rounded-md p-[5px]"
-                onChange={handleChange}
-              />
-            </div>
-
-            <div>
-              <label for="last-name" className="mt-[10px] mr-[10px]">
-                Last Name
-              </label>
-              <input
-                value={userValue?.lastName}
-                id="last-name"
-                name="lastName"
-                className="text-sm w-[160px] border border-gray-300 rounded-md p-[5px]"
-                onChange={handleChange}
-              />
-            </div>
-          </div>
-
-          <label for="bio" className="mt-[10px]">
-            Bio
-          </label>
-          <textarea
-            id="bio"
-            name="bio"
-            value={userValue?.bio}
-            className="text-sm resize-none border border-gray-300 rounded-md p-[5px]"
-            onChange={handleChange}
-          ></textarea>
-          <label for="website" className="mt-[10px]">
-            Website
-          </label>
-          <input
-            value={userValue?.website}
-            id="website"
-            name="website"
-            className="text-sm border border-gray-300 rounded-md p-[5px]"
-            onChange={handleChange}
-          />
-          <div className="mt-[15px]">
-            <input
-              className="mr-[10px] bg-primary-color text-white px-[15px] py-[5px] rounded-md border border-primary-color cursor-pointer hover:bg-primary-dark hover:border-primary-dark"
-              value="Save"
-              type="submit"
+        <div
+          style={{ filter: showAvatarModal ? "blur(10px)" : "", ...style }}
+          className="bg-white z-40 p-[20px] rounded-lg"
+        >
+          <h1 className="font-bold mb-[15px]">EDIT PROFILE</h1>
+          <form className="flex flex-col" onSubmit={submitHandler}>
+            <img
+              src={userValue?.avatarUrl}
+              alt="avatar"
+              className="w-[100px] h-[100px] m-auto rounded-full"
             />
+
             <button
-              className="bg-white px-[15px] py-[5px] border border-primary-color rounded-md text-primary-color hover:bg-primary-color hover:text-white"
-              onClick={() => setShowEditModal(false)}
+              onClick={(e) => {
+                e.preventDefault();
+                setShowAvatarModal(true);
+              }}
+              className="bg-primary-color text-white my-[6px] mb-[20px] p-[7px] hover:bg-primary-dark"
             >
-              Discard
+              Update Avatar
             </button>
-          </div>
-        </form>
-      </div>
+            <div className="flex xs:flex-col">
+              <div className="xs:mb-[10px]">
+                <label className="xs:mr-[10px]" for="first-name">
+                  First Name
+                </label>
+                <input
+                  value={userValue?.firstName}
+                  id="first-name"
+                  name="firstName"
+                  className="text-sm w-[160px] border border-gray-300 rounded-md p-[5px]"
+                  onChange={handleChange}
+                />
+              </div>
+
+              <div>
+                <label for="last-name" className="mt-[10px] mr-[10px]">
+                  Last Name
+                </label>
+                <input
+                  value={userValue?.lastName}
+                  id="last-name"
+                  name="lastName"
+                  className="text-sm w-[160px] border border-gray-300 rounded-md p-[5px]"
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+
+            <label for="bio" className="mt-[10px]">
+              Bio
+            </label>
+            <textarea
+              id="bio"
+              name="bio"
+              value={userValue?.bio}
+              className="text-sm resize-none border border-gray-300 rounded-md p-[5px]"
+              onChange={handleChange}
+            ></textarea>
+            <label for="website" className="mt-[10px]">
+              Website
+            </label>
+            <input
+              value={userValue?.website}
+              id="website"
+              name="website"
+              className="text-sm border border-gray-300 rounded-md p-[5px]"
+              onChange={handleChange}
+            />
+            <div className="mt-[15px]">
+              <input
+                className="mr-[10px] bg-primary-color text-white px-[15px] py-[5px] rounded-md border border-primary-color cursor-pointer hover:bg-primary-dark hover:border-primary-dark"
+                value="Save"
+                type="submit"
+              />
+              <button
+                className="bg-white px-[15px] py-[5px] border border-primary-color rounded-md text-primary-color hover:bg-primary-color hover:text-white"
+                onClick={() => setShowEditModal(false)}
+              >
+                Discard
+              </button>
+            </div>
+          </form>
+        </div>
+      </Modal>
     </>
   );
 };
--- src/components/FollowCount.jsx
+++ src/components/FollowCount.jsx
@@ -1,51 +1,71 @@
 import { useNavigate } from "react-router";
+import { Modal } from "@mui/material";
 import { usePost } from "../context/post-context";
 
 export const FollowCount = ({ arr, setShowModal, showModal }) => {
   const { getUserPost } = usePost();
   const navigate = useNavigate();
+
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 340,
+    bgcolor: "background.paper",
+    boxShadow: 24,
+    p: 2,
+  };
+
   return (
     <>
-      <div className="w-[300px] bg-primary-lightest border p-[15px] absolute top-44 shadow-md z-30 rounded-xl left-[40%] md:left-[30%] sm:left-[20%] xs:w-[250px] xs:left-[15%]">
-        <div className="flex justify-between items-center pb-[10px]">
-          <p className="font-bold">{showModal.type}</p>
-          <i
-            className="fa-solid fa-xmark cursor-pointer hover:text-primary-color"
-            onClick={() =>
-              setShowModal((showModal) => ({
-                ...showModal,
-                show: false,
-                type: "",
-              }))
-            }
-          ></i>
-        </div>
+      <Modal
+        open={showModal}
+        onClose={() => setShowModal(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <div style={{ ...style }} className="bg-primary-lightest rounded-lg p-[20px]">
+          <div className="flex justify-between items-center pb-[10px]">
+            <p className="font-bold">{showModal.type}</p>
+            <i
+              className="fa-solid fa-xmark cursor-pointer hover:text-primary-color"
+              onClick={() =>
+                setShowModal((showModal) => ({
+                  ...showModal,
+                  show: false,
+                  type: "",
+                }))
+              }
+            ></i>
+          </div>
 
-        <div>
-          {arr.length === 0 && <p>No {showModal.type}</p>}
-          {arr.map(({ _id, firstName, lastName, username, avatarUrl }) => (
-            <div
-              key={_id}
-              onClick={() => {
-                getUserPost(username);
-                navigate(`/profile/${username}`);
-                setShowModal((showModal) => ({ ...showModal, show: false }));
-              }}
-              className="flex cursor-pointer my-[10px] shadow-md p-[10px]"
-            >
-              <img
-                src={avatarUrl}
-                alt="avatar"
-                className="w-[35px] h-[35px] mr-2 bg-primary-color rounded-full"
-              />
-              <div>
-                <h1 className="font-medium text-sm">{`${firstName} ${lastName}`}</h1>
-                <p className="text-xs">@{username}</p>
+          <div>
+            {arr.length === 0 && <p>No {showModal.type}</p>}
+            {arr.map(({ _id, firstName, lastName, username, avatarUrl }) => (
+              <div
+                key={_id}
+                onClick={() => {
+                  getUserPost(username);
+                  navigate(`/profile/${username}`);
+                  setShowModal((showModal) => ({ ...showModal, show: false }));
+                }}
+                className="flex cursor-pointer my-[10px] shadow-md p-[10px]"
+              >
+                <img
+                  src={avatarUrl}
+                  alt="avatar"
+                  className="w-[35px] h-[35px] mr-2 bg-primary-color rounded-full"
+                />
+                <div>
+                  <h1 className="font-medium text-sm">{`${firstName} ${lastName}`}</h1>
+                  <p className="text-xs">@{username}</p>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>
+      </Modal>
     </>
   );
 };
