d1ae7b8ab071f64420e9e1df2d1a7ef1e22ab6de
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-28 19:56:16 +0530

    feat: add components in modal

--- src/components/EditDeleteModal.jsx
+++ src/components/EditDeleteModal.jsx
@@ -1,19 +1,54 @@
-export const EditDeleteModal = ({ editPost, deletePost }) => {
+import { useAuth } from "../context/auth-context";
+import { handleCopyLink } from "../utils/handleCopyLink";
+import { handleShare } from "../utils/handleShare";
+
+export const EditDeleteModal = ({
+  editPost,
+  deletePost,
+  postId,
+  username,
+  setIsModalVisible,
+}) => {
+  const { authState } = useAuth();
   return (
-    <div className="text-sm bg-white p-3 w-[120px] shadow-lg absolute right-2 top-12">
+    <div className="text-sm bg-white p-3 w-[150px] shadow-lg absolute right-2 top-12">
       <p
-        onClick={editPost}
+        onClick={() => {
+          handleCopyLink(postId);
+          setIsModalVisible(false);
+        }}
         className="my-2 cursor-pointer hover:text-primary-color"
       >
-        <i className="fa-solid fa-pencil"></i> Edit Post
+        <i className="fa-solid fa-link"></i> Copy Link
       </p>
       <hr />
       <p
+        onClick={() => {
+          handleShare(postId);
+          setIsModalVisible(false);
+        }}
         className="my-2 cursor-pointer hover:text-primary-color"
-        onClick={deletePost}
       >
-        <i className="fa-solid fa-trash-can"></i> Delete Post
+        <i className="fa-solid fa-share"></i> Share
       </p>
+      {authState?.user?.username === username && (
+        <div>
+          <hr />
+          <p
+            onClick={editPost}
+            className="my-2 cursor-pointer hover:text-primary-color"
+          >
+            <i className="fa-solid fa-pencil"></i> Edit Post
+          </p>
+          <hr />
+          <p
+            className="my-2 cursor-pointer hover:text-red-500"
+            onClick={deletePost}
+          >
+            <i className="fa-solid fa-trash-can"></i> Delete Post
+          </p>
+        </div>
+      )}
     </div>
   );
 };
