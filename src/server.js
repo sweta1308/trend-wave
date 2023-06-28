efd67079cd48132fec2773919285bf201c38eaee
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-28 19:59:30 +0530

    feat: add comment apis

--- src/server.js
+++ src/server.js
@@ -16,6 +16,14 @@ import {
   getAllUserPostsHandler,
 } from "./backend/controllers/PostController";
 import {
+  getPostCommentsHandler,
+  addPostCommentHandler,
+  editPostCommentHandler,
+  deletePostCommentHandler,
+  upvotePostCommentHandler,
+  downvotePostCommentHandler,
+} from "./backend/controllers/CommentController";
+import {
   followUserHandler,
   getAllUsersHandler,
   getUserHandler,
@@ -69,6 +77,28 @@ export function makeServer({ environment = "development" } = {}) {
       this.post("/posts/edit/:postId", editPostHandler.bind(this));
       this.post("/posts/like/:postId", likePostHandler.bind(this));
       this.post("/posts/dislike/:postId", dislikePostHandler.bind(this));
+
+      //post comments routes (public)
+      this.get("/comments/:postId", getPostCommentsHandler.bind(this));
+
+      //post comments routes (private)
+      this.post("/comments/add/:postId", addPostCommentHandler.bind(this));
+      this.post(
+        "/comments/edit/:postId/:commentId",
+        editPostCommentHandler.bind(this)
+      );
+      this.post(
+        "/comments/delete/:postId/:commentId",
+        deletePostCommentHandler.bind(this)
+      );
+      this.post(
+        "/comments/upvote/:postId/:commentId",
+        upvotePostCommentHandler.bind(this)
+      );
+      this.post(
+        "/comments/downvote/:postId/:commentId",
+        downvotePostCommentHandler.bind(this)
+      );
 
       // user routes (public)
       this.get("/users", getAllUsersHandler.bind(this));
