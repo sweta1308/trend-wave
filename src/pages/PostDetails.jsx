ac0a96d31617b57ccd9f94f4d2c4ecc9815d1bbb
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-28 19:59:10 +0530

    feat: add "add comments" feature

--- src/pages/PostDetails.jsx
+++ src/pages/PostDetails.jsx
@@ -8,15 +8,20 @@ import { RightNav } from "../components/RightNav";
 import { DisplayPost } from "../components/DisplayPost";
 import { useUser } from "../context/user-context";
 import { usePost } from "../context/post-context";
+import { useComment } from "../context/comment-context";
+import { useAuth } from "../context/auth-context";
 
 export const PostDetails = () => {
   document.title = "Trend Wave | Post details";
   const [postDetails, setPostDetails] = useState({});
   const [postLoading, setPostLoading] = useState(false);
   const { postID } = useParams();
+  const { authState } = useAuth();
   const { userState } = useUser();
   const navigate = useNavigate();
   const { getUserPost, postState } = usePost();
+  const { addComments, deleteComment } = useComment();
+  const [commentInput, setCommentInput] = useState("");
 
   const getPostDetails = async () => {
     try {
@@ -33,6 +38,8 @@ export const PostDetails = () => {
       console.log(e);
     }
   };
+
+  console.log(commentInput);
 
   useEffect(() => {
     getPostDetails();
@@ -53,6 +60,24 @@ export const PostDetails = () => {
           ) : (
             <div>
               <DisplayPost userPost={postDetails} />
+              <div className="mx-auto my-[15px]">
+                <input
+                  value={commentInput}
+                  onChange={(e) => setCommentInput(e.target.value)}
+                  placeholder="Add Comment..."
+                  className="w-[505px] border border-gray-400 px-[15px] py-[10px] text-sm rounded-lg lg:w-[405px] md:w-[255px] xs:w-[220px]"
+                />
+                <button
+                  onClick={() => {
+                    addComments(postID, commentInput);
+                    setCommentInput("");
+                  }}
+                  className="bg-primary-color ml-[10px] text-white p-[10px] text-sm rounded-lg hover:bg-primary-dark"
+                >
+                  Comment
+                </button>
+              </div>
+
               {postDetails?.comments?.length > 0 ? (
                 <div className="my-2 rounded-xl text-[13px]">
                   {postDetails?.comments?.map((comment) => {
@@ -62,10 +87,10 @@ export const PostDetails = () => {
                     return (
                       <div
                         key={comment?._id}
-                        className="bg-white px-5 py-px pt-2"
+                        className="bg-white px-5 py-px pt-2 rounded-lg"
                       >
                         <div
-                          className="flex cursor-pointer"
+                          className="flex items-center cursor-pointer"
                           onClick={() => {
                             getUserPost(userComment?.username);
                             navigate(`/profile/${userComment?.username}`);
@@ -81,7 +106,18 @@ export const PostDetails = () => {
                             <p className="text-xs">@{userComment?.username}</p>
                           </div>
                         </div>
-                        <p className="my-2">{comment?.text}</p>
+                        <div className="flex justify-between items-center">
+                          <p className="my-2">{comment?.text}</p>
+                          {authState?.user?.username ===
+                            userComment?.username && (
+                            <i
+                              onClick={() =>
+                                deleteComment(postDetails?._id, comment?._id)
+                              }
+                              className="fa-solid fa-trash-can cursor-pointer hover:text-primary-color"
+                            ></i>
+                          )}
+                        </div>
                       </div>
                     );
                   })}
