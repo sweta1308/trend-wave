f631bd9479fbd88644e71c40ef3ca7d4d7fb9395
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-29 20:01:53 +0530

    feat: add dark theme condition

--- src/context/theme-context.js
+++ src/context/theme-context.js
@@ -1,10 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { createContext, useContext } from "react";
 
 const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(
+    localStorage.getItem("theme") === "dark" ? true : false
+  );
+
+  useEffect(() => {
+    localStorage.setItem("theme", `${isDarkMode ? "dark" : "light"}`);
+    const activeTheme = localStorage.getItem("theme");
+    if (activeTheme === "light") {
+      document.documentElement.classList.add("light");
+      document.documentElement.classList.remove("dark");
+    } else {
+      document.documentElement.classList.add("dark");
+      document.documentElement.classList.remove("light");
+    }
+  }, [isDarkMode]);
   return (
     <ThemeContext.Provider value={{ isDarkMode, setIsDarkMode }}>
       {children}
