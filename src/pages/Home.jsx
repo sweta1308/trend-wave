f2b72752f85c3b2fed509ffc9205c5f964de3032
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-30 12:46:17 +0530

    refactor: made minor changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -47,19 +47,21 @@ export const Home = () => {
         <Sidenav />
         <div className="min-h-screen bg-primary-lightest py-5 mx-5 relative left-[15%] w-[65%] flex flex-col items-center rounded-xl lg:left-[30%] lg:w-[65%] md:left-0 md:w-full md:pb-[110px] dark:bg-dark-light">
           <FilterComponent />
-          {postState.postLoading && (
+          {postState.postLoading ? (
             <PulseLoader color="var(--primary-color)" size={30} />
+          ) : (
+            <div className="mt-[30px]">
+              {userFeed.length === 0 ? (
+                <h1 className="font-bold text-3xl">No Posts to show.</h1>
+              ) : (
+                userFeed?.map((posts) => (
+                  <div key={posts._id}>
+                    <DisplayPost userPost={posts} />
+                  </div>
+                ))
+              )}
+            </div>
           )}
-          <div className="mt-[30px]">
-            {userFeed.length === 0 && (
-              <h1 className="font-bold text-3xl">No Posts to show.</h1>
-            )}
-            {userFeed?.map((posts) => (
-              <div key={posts._id}>
-                <DisplayPost userPost={posts} />
-              </div>
-            ))}
-          </div>
         </div>
         <RightNav />
       </div>
