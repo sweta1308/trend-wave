1e22745e4100e89f9bf84e9ff70256c71b1d221c
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-07-01 16:58:21 +0530

    feat: add user feat

--- src/reducer/user-reducer.js
+++ src/reducer/user-reducer.js
@@ -2,6 +2,8 @@ export const userReducer = (state, { type, payload }) => {
   switch (type) {
     case "GET_USER":
       return (state = payload);
+    case "ADD_USER":
+      return (state = [...state, payload]);
     case "UPDATE_USER":
       return state.map((user) =>
         user._id === payload._id ? { ...payload } : user
