72e3341eec9a865af01fa25719025f5f852b26b6
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-07-01 16:54:54 +0530

    feat: add react hot toast

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router";
-import { ToastContainer } from "react-toastify";
+import { Toaster } from "react-hot-toast";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 import { RequireAuth } from "./components/RequireAuth";
@@ -12,11 +12,20 @@ import { Explore } from "./pages/Explore";
 import { Profile } from "./pages/Profile";
 import { Bookmark } from "./pages/Bookmark";
 import { PostDetails } from "./pages/PostDetails";
+import { PageNotFound } from "./pages/404";
 
 function App() {
   return (
     <div className="App dark:bg-dark-mode dark:text-white">
-      <ToastContainer position="top-right" autoClose={2000} />
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+        containerStyle={{
+          top: "1rem",
+          right: "1rem",
+          fontSize: "0.9rem",
+        }}
+      />
       <Routes>
         <Route path="/register" element={<Header />} />
         <Route path="/login" element={<Login />} />
@@ -29,6 +38,7 @@ function App() {
           <Route path="/profile/:username" element={<Profile />} />
         </Route>
         <Route path="/mockman" element={<Mockman />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </div>
   );
